ec35d8181a9714c8eb1a50b4a6a4d56ca9d41149
erfan <erfank20041382@gmail.com>
2025-12-03 01:54:52 +0330

    creating lifespan

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,28 @@
 
-from db.base import Base, engine, get_session
-from db.models import User, Thread, Message
-from services.openai_service import OpenAIService
-from schemas.message import MessageIn,MessageOut
+from db.base import Base, engine
 from routes.auth import router as auth_router
 from fastapi import FastAPI
 from routes.chat import router as chat_router
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+import uvicorn
 
-app = FastAPI(title="ESG Buddy Backend with ORM")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup logic
+    async with engine.begin() as conn:
+        # await conn.run_sync(Base.metadata.drop_all)   # optional
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield   # Application runs here
+
+    # Shutdown logic (optional)
+    # e.g., closing resources
+
+
+app = FastAPI(title="ESG Buddy Backend with ORM",lifespan=lifespan)
 
 
 app.add_middleware(
@@ -23,10 +37,5 @@ app.include_router(auth_router)
 app.include_router(chat_router)
 
 
-@app.on_event("startup")
-async def startup():
-    async with engine.begin() as conn:
-        # # Drop all tables first
-        # await conn.run_sync(Base.metadata.drop_all)
-        # Then recreate them
-        await conn.run_sync(Base.metadata.create_all)
+if __name__ == "__main__":
+    uvicorn.run("main:app",host="0.0.0.0",port=8000,reload=True)
