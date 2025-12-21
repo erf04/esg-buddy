e9fd7a3703d9295a209e87ad1b29efff7758f74c
erfan <erfank20041382@gmail.com>
2025-12-22 01:22:28 +0330

    add uvicorn run

--- backend/main.py
+++ backend/main.py
@@ -39,5 +39,5 @@ app.include_router(chat_router)
 app.include_router(thread_router)
 
 
-# if __name__ == "__main__":
-#     uvicorn.run("main:app",host="0.0.0.0",port=8000,reload=True,log_level="trace")
+if __name__ == "__main__":
+    uvicorn.run("main:app",host="0.0.0.0",port=8000,reload=True,log_level="debug")
